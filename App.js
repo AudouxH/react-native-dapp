c9e5ffd0fef7d52b2b167475fc340c47eda45bf8
Audouxh <hugo.audoux@epitech.eu>
2023-08-29 16:02:57 +0200

    adding basic react native app

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={{}}>
+      <Text>Basic react native app</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
